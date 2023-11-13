e27b1e9534f03e8ef0adaf54166b7d0d9f5e01f9
moong2 <www.castlehi@gmail.com>
2023-11-13 20:42:15 +0900

    feat: implement total order amount before discount calculator

--- src/main/java/christmas/model/calculator/OrderCalculator.java
+++ src/main/java/christmas/model/calculator/OrderCalculator.java
@@ -1,2 +1,33 @@
-package christmas.model.calculator;public class OrderCalculator {
+package christmas.model.calculator;
+
+import static christmas.util.exceptions.Exceptions.ORDERING_INVALID;
+import static christmas.util.menu.MenuList.NONE_MENU;
+
+import christmas.util.menu.MenuList;
+import christmas.util.menu.MenuUtils;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class OrderCalculator {
+    public static int totalOrderAmountBeforeDiscount(Map<String, Integer> orders) {
+        int totalOrderAmountBeforeDiscount = 0;
+
+        for (Entry<String, Integer> order : orders.entrySet()) {
+            String menuName = order.getKey();
+            MenuList menu = MenuUtils.getMenuByName(menuName);
+
+            menuCheck(menu);
+
+            Integer numberOfFood = order.getValue();
+            totalOrderAmountBeforeDiscount += (menu.getPrice() * numberOfFood);
+        }
+
+        return totalOrderAmountBeforeDiscount;
+    }
+
+    private static void menuCheck(MenuList menu) {
+        if (menu == NONE_MENU) {
+            throw new IllegalArgumentException(ORDERING_INVALID.getMessage());
+        }
+    }
 }
