71bc4c00d6fc608b7d21beaaccca20aa98ccdbd2
moong2 <www.castlehi@gmail.com>
2023-11-14 01:36:18 +0900

    refactor: make event category to enum

--- src/main/java/christmas/util/calendar/EventCategory.java
+++ src/main/java/christmas/util/calendar/EventCategory.java
@@ -1,2 +1,19 @@
-package christmas.util.calendar;public enum EventCategory {
+package christmas.util.calendar;
+
+public enum EventCategory {
+    CHRISTMAS_DDAY_EVENT("크리스마스 디데이 할인"),
+    WEEKDAY_EVENT("평일 할인"),
+    WEEKEND_EVENT("주말 할인"),
+    SPECIAL_EVENT("특별 할인"),
+    GIVE_EVENT("증정 이벤트");
+
+    private final String category;
+
+    EventCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
