9577b1be7a9eb9a060ffa6737ecea080917a973f
moong2 <www.castlehi@gmail.com>
2023-11-13 22:24:17 +0900

    refactor: make given menu standard money to enum

--- src/main/java/christmas/util/calendar/EventDetails.java
+++ src/main/java/christmas/util/calendar/EventDetails.java
@@ -1,2 +1,15 @@
-package christmas.util.calendar;public enum EventDetails {
+package christmas.util.calendar;
+
+public enum EventDetails {
+    GIFT_MENU_EVENT(120_000);
+
+    private final int money;
+
+    EventDetails(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
