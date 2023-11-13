38796458ef5668c6da83c7b01b792e52384d635e
moong2 <www.castlehi@gmail.com>
2023-11-14 01:33:55 +0900

    test: total discount calculator

--- src/test/java/christmas/model/calculator/OrderCalculatorTest.java
+++ src/test/java/christmas/model/calculator/OrderCalculatorTest.java
@@ -1,7 +1,12 @@
 package christmas.model.calculator;
 
+import static christmas.util.calendar.EventCategory.CHRISTMAS_DDAY_EVENT;
+import static christmas.util.calendar.EventCategory.GIVE_EVENT;
+import static christmas.util.calendar.EventCategory.SPECIAL_EVENT;
+import static christmas.util.calendar.EventCategory.WEEKDAY_EVENT;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.util.calendar.EventCategory;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
@@ -41,6 +46,25 @@ class OrderCalculatorTest {
                 Arguments.of(Map.of("시저샐러드", 1), Map.of(8000, 1)),
                 Arguments.of(Map.of("양송이수프", 1, "바비큐립", 1, "초코케이크", 1, "제로콜라", 1), Map.of(6000, 1, 54000, 1, 15000, 1, 3000, 1)),
                 Arguments.of(Map.of("타파스", 5, "티본스테이크", 5, "아이스크림", 5, "레드와인", 5), Map.of(5500, 5, 55000, 5, 5000, 5, 60000, 5))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("totalDiscount")
+    @DisplayName("혜택 내역(증정 메뉴 + 할인 내역)에 따라 총혜택 금액이 반환된다.")
+    void totalDiscountTest(Map<EventCategory, Integer> discounts, int totalDiscount) {
+        // given
+        // when
+        int testTotalDiscount = orderCalculator.getTotalDiscount(discounts);
+
+        // then
+        assertThat(testTotalDiscount).isEqualTo(totalDiscount);
+    }
+
+    static Stream<Arguments> totalDiscount() {
+        return Stream.of(
+                Arguments.of(Map.of(CHRISTMAS_DDAY_EVENT.getCategory(), 1200, WEEKDAY_EVENT.getCategory(), 4046,
+                        SPECIAL_EVENT.getCategory(), 1000, GIVE_EVENT.getCategory(), 25000), 31246)
         );
     }
 }
