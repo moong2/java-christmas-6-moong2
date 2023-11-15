cdb05fb5eb758595f0bd1ceaa24da0169130d1d0
moong2 <www.castlehi@gmail.com>
2023-11-15 14:40:24 +0900

    feat: implement total sales amount calculator method

--- src/main/java/christmas/model/calculator/StatisticCalculator.java
+++ src/main/java/christmas/model/calculator/StatisticCalculator.java
@@ -1,2 +1,32 @@
-package christmas.model.calculator;public class statisticCalculator {
+package christmas.model.calculator;
+
+import christmas.model.domain.Client;
+import java.util.List;
+
+public class StatisticCalculator {
+
+    private static final int ANTICIPATE_PERCENT = 5;
+    private static final double PERCENT = 100.0;
+    private static final double ROUND = 100.0;
+
+    public static int numberOfClients(List<Client> clients) {
+        return clients.size();
+    }
+
+    public static double nextEventNumberOfClients(List<Client> clients) {
+        int numberOfClients = numberOfClients(clients);
+        double nextEventNumberOfClients = numberOfClients * (ANTICIPATE_PERCENT / PERCENT);
+
+        return Math.round(nextEventNumberOfClients * ROUND) / ROUND;
+    }
+
+    public static long totalSalesAmount(List<Client> clients) {
+        long totalSalesAmount = 0L;
+
+        for (Client client : clients) {
+            totalSalesAmount += client.getTotalAmountBeforeDiscount();
+        }
+
+        return totalSalesAmount;
+    }
 }
